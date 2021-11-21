cd5e00f6df136dba651bc2a8584d466ff24e6895
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2021-11-21 17:18:35 +0100

    create comment with prisma

--- graphql-basics/src/resolvers/Comment.ts
+++ graphql-basics/src/resolvers/Comment.ts
@@ -3,42 +3,43 @@ import {
   Comment, Post, Resolvers, User,
 } from '../schema';
 import { CommentsDataType, PostDataType, UserDataType } from '../db';
+import { Context } from '../context';
 
 const comment: Resolvers = {
   Query: {
     comments: (parent, args, { db }) => db.comments,
   },
   Mutation: {
-    createComment: async (parent, { comment: { body, post, author } }, { db, pubsub }, info) => {
-      const userExist = db.users.some(({ id }: UserDataType) => id === author);
-      const postExist = db.posts.some(({ id, published }: PostDataType) => id === post && published);
-      if (!userExist) throw new Error('User not found');
-      if (!postExist) throw new Error('Post not found');
-
-      const id = uuidv4();
-      const newComment: CommentsDataType = {
-        id,
-        body,
-        author,
-        post,
-      };
-
-      if (postExist) {
-        // enrich the concerned post with the new comment on database
-        const commentedPost = db.posts.find(({ id: postId }: { id: string }) => postId === post);
-        commentedPost.comments.push(id);
-
-        // enrich comments table with the new comment on database
-        db.comments.push(newComment);
-      }
-      pubsub.publish(`comment ${post}`, {
-        comment: {
-          mutation: 'CREATED',
-          data: newComment,
+    createComment: async (parent, { comment: { body, post, author } }, { db, pubsub, prisma }: Context, info) => {
+      const authorOfTheComment = await prisma.user.findUnique({
+        where: {
+          id: author,
         },
       });
-      return newComment as unknown as Comment;
-    },
+      if (!authorOfTheComment) {
+        throw new Error('No user found');
+      }
+      const commentTemp = await prisma.comment.create({
+        data: {
+          authorId: author,
+          postId: post,
+          body,
+        },
+      });
+
+      // pubsub.publish(`comment ${post}`, {
+      //   comment: {
+      //     mutation: 'CREATED',
+      //     data: newComment,
+      //   },
+      // });
+
+      return commentTemp;
+
+      return ({} as unknown as any);
+    }
+
+    ,
   },
   Comment: {
     author: (parent, args, { db }) => {
