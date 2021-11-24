e090b6f6d884967e61e0016b53802adb28263d47
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2021-11-24 19:35:15 +0100

    Remove hard coded data

--- graphql-basics/src/context.ts
+++ graphql-basics/src/context.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { PubSub } from 'graphql-subscriptions';
-import db, { DB } from './db';
 import { prisma } from '../prisma/client';
 
 export interface Context {
-  db: DB
   prisma: PrismaClient
 }
 
 export const pubsub = new PubSub();
 
-export const context: Context = { db, prisma };
+export const context: Context = { prisma };
